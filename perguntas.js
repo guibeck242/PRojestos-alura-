9e01fba330c26894eda746fb4629132604b1d940
guibeck242 <guilhermebeck2@gmail.com>
2025-06-04 22:59:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Qual o país mais novo do mundo?',
+    'Sudão do Sul'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'Qual o lugar mais profundo dos oceanos?',
+    'Fossa das Marianas'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
